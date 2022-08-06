f8b85cb2473ada8b3558624c9b2d051fdca105ce
Valerie Pomerleau <v.pomerleau@gmail.com>
2022-08-06 12:03:47 -0700

    retrieve search params from purchase confirmation URL and outline next steps in comments

--- src/components/booking-result/booking-result.js
+++ src/components/booking-result/booking-result.js
@@ -1,19 +1,39 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 import { PageLayout } from "../page-layout";
-import './booking-result.scss';
+import "./booking-result.scss";
 
 export const BookingResult = () => {
-  const clientSecret = new URLSearchParams(window.location.search).get(
+  const searchParams = useLocation().search;
+
+  const paymentIntentId = new URLSearchParams(searchParams).get(
+    "payment_intent"
+  );
+
+  const clientSecret = new URLSearchParams(searchParams).get(
     "payment_intent_client_secret"
   );
-  const status = new URLSearchParams(window.location.search).get(
+  const status = new URLSearchParams(searchParams).get(
     "redirect_status"
   );
+
+  // show loader - payment processing
+
+  // paymentIntent object from stripe
+  // create rider id
+  // log transaction in database
+  // log rider products in database
+
+  // redirect to purchase confirmation page
+
+  // on purchase confirmation page
+  // show products purchased
+  // provide link to view/manage bookings
 
   return (
     <PageLayout>
       <div className="booking-result__container">
-        {clientSecret && <div id="payment-message">{status}</div>}
+        {clientSecret && <div id="payment-message">{paymentIntentId}: {status}</div>}
       </div>
     </PageLayout>
   );
