ee7ea8e78351f37c510e226ca6a8f270b81d6739
Valerie Pomerleau <v.pomerleau@gmail.com>
2022-08-09 08:33:42 -0700

    modifications to featured content section layout and copy

--- src/components/feature-content/bike-park-feature.js
+++ src/components/feature-content/bike-park-feature.js
@@ -1,12 +1,12 @@
 import React from "react";
 import "./bike-park-feature.scss";
-import { Card } from "@mui/material";
+import { Card, Grid } from "@mui/material";
 
 export const BikeParkFeature = ({ title, description, resourceUrl, icon }) => (
-  <Card className="bikepark-feature">
-    <h3 className="bikepark-feature__headline">
-      {title}
-    </h3>
-    <p className="bikepark-feature__description">{description}</p>
-  </Card>
+  <Grid item xs={12} md={6}>
+    <Card className="bikepark-feature">
+      <h3 className="bikepark-feature__headline">{title}</h3>
+      <p className="bikepark-feature__description">{description}</p>
+    </Card>
+  </Grid>
 );
--- src/components/feature-section/bike-park-features.js
+++ src/components/feature-section/bike-park-features.js
@@ -2,38 +2,23 @@ import React from "react";
 import { BikeParkFeature } from "../feature-content/bike-park-feature";
 import "./bike-park-features.scss";
 import { PageLoader } from "../animation-bike/page-loader";
+import { Grid } from "@mui/material";
 
 export const BikeParkFeatures = () => {
   const featuresList = [
     {
       id: "1",
-      title: "Bike Park",
+      title: "Riding for all ages, all riding levels",
       description:
-        "This route is worth riding MANY times. From here the trail narrows and skirts a rocky hillside. Great trail, with amazing views, but may see a lot of traffic. IMO it is a better ride to turn around and follow the same trail down.",
+        "The bike park is designed so that 80% of the park is rideable by 80% of riders. The remaining 20% includes toddler-specific learning areas and big jumps with air bags for advanced riders.",
       resourceUrl: "",
       icon: "",
     },
     {
       id: "2",
-      title: "Bike Park",
+      title: "Talk to us about hosting your event",
       description:
-        "At the bottom the trail joins Hide and Seek above a major creek crossing. Jewel of the Amasa Back area, relatively new to the other trails. Only about 25% of the trail is good to ride. This created a reroute of the single track.",
-      resourceUrl: "",
-      icon: "",
-    },
-    {
-      id: "3",
-      title: "Bike Park",
-      description:
-        "A long, long, did I mention long? Fire road that circles Mauna Kea. I discovered this trail while out with my friend one day. Use some caution in the corners here. Don't miss this trail!",
-      resourceUrl: "",
-      icon: "",
-    },
-    {
-      id: "4",
-      title: "Bike Park",
-      description:
-        "Sadly, a better trail in this same zone (Barney Lake) is designated wilderness and is therefore 'unlawful' to ride. This is a legitimate expert-level trail, but much of the difficulty can be mitigated by slowing down and picking your line carefully. Mostly dry with some small mud puddles. The decent is even more steep and exposed.",
+        "Woodwork Bike Park offers turnkey solutions for birthday parties, team building activities. We have reservable group rooms, bike rentals, and activity guides - and a large outdoor space in addition to our indoor riding area.",
       resourceUrl: "",
       icon: "",
     },
@@ -44,7 +29,7 @@ export const BikeParkFeatures = () => {
       <h2 className="bikepark-features__title">
         Explore Woodwork Bike Park Features
       </h2>
-      <div className="bikepark-features__grid">
+      <Grid container spacing={2} alignItems='stretch' sx={{mb:'0', pb:'0'}}>
         {featuresList.map((feature) => (
           <BikeParkFeature
             key={feature.id}
@@ -54,7 +39,7 @@ export const BikeParkFeatures = () => {
             icon="fa-solid fa-bicycle"
           />
         ))}
-      </div>
+      </Grid>
       <div className="bike__container">
         <PageLoader />
       </div>
