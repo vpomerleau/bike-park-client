339a4e039fda7cbf23e8d190700cc2c49def66a3
Valerie Pomerleau <v.pomerleau@gmail.com>
2022-08-15 15:19:07 -0700

    remove basename from browserrouter

--- src/index.js
+++ src/index.js
@@ -3,16 +3,16 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Auth0ProviderWithHistory } from "./auth0-provider-with-history";
 import { App } from "./App";
-import './global-styles.scss';
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
+import "./global-styles.scss";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/index.html">
+    <BrowserRouter>
       <Auth0ProviderWithHistory>
         <App />
       </Auth0ProviderWithHistory>
